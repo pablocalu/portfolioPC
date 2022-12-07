cbc1e6b587700fc56982461f8e531fcc6f02e1ab
Pablo Carro <pablo.carro@live.com>
2022-12-06 22:40:07 -0300

    today test

--- myportf/src/App.js
+++ myportf/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div>
       <NavBar/>
+      <Home/>
     </div>
   );
 }
--- myportf/src/Components/NavBar.js
+++ myportf/src/Components/NavBar.js
@@ -1,8 +1,18 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
 
 export default function NavBar(){
     return (
-        <div>
-            Hola
-        </div>
+        <Box>
+            <Box sx={{backgroundColor: 'red', width:'5vw', height: '100vh', display:'flex', flexDirection:'column', justifyContent:'space-between', alignItems: 'center', position: 'fixed'}}>
+                <Box>Logo</Box>
+                <Typography sx={{writingMode: 'vertical-lr', transform: 'rotate(180deg)', width:'5vw', backgroundColor: 'green' }}>Proyectos</Typography>
+                <Typography sx={{writingMode: 'vertical-lr', transform: 'rotate(180deg)', width:'5vw'}}>Sobre Mi</Typography>
+                <Typography sx={{writingMode: 'vertical-lr', transform: 'rotate(180deg)', width:'5vw'}}>Contacto</Typography>
+                <Typography sx={{writingMode: 'vertical-lr', transform: 'rotate(180deg)', width:'5vw'}}>LinkedIn</Typography>
+                <Typography sx={{writingMode: 'vertical-lr', transform: 'rotate(180deg)', width:'5vw'}}>Github</Typography>
+            </Box>
+        </Box>
     )
 }
